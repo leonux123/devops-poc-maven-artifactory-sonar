244295edb6f78f562bf5dac90f14ded15f932566
leonux123 <42426813+leonux123@users.noreply.github.com>
2019-01-13 17:26:53 -0600

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Bye World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Bye World!\"");
+            fail("\"message\" is not \"Hello World!\"");
         }
     }
 
